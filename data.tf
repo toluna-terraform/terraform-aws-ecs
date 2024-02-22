9c172d0b6a666afb8b77fb74dfb6199729827eaf
waseemkh88 <124219085+waseemkh88@users.noreply.github.com>
2024-02-22 16:21:54 +0200

    PLM-1535 - changes the places where we render templates using the old method and refactor it to use the non-deprecated method

--- data.tf
+++ data.tf
@@ -23,34 +23,3 @@ data "external" "current_service_image" {
     aws_profile = "${var.aws_profile}"
   }
 }
-
-# Container definitions
-data "template_file" "default-container" {
-
-  template = file("${path.module}/templates/containers.json")
-  vars = {
-    region                = data.aws_region.current.name
-    log_group             = var.aws_cloudwatch_log_group_name
-    cpu                   = var.app_container_cpu
-    memory                = var.app_container_memory
-    container_port        = var.app_container_port
-    dockerLabels          = local.dockerLabels == "{}" ? "null" : local.dockerLabels
-    task_execution_role   = aws_iam_role.ecs_task_execution_role.arn
-    name                  = "${var.app_name}-${local.env_name}"
-    image                 = data.external.current_service_image.result.image
-    environment           = local.app_container_environment == "[]" ? "null" : local.app_container_environment
-    secrets               = local.app_container_secrets == "[]" ? "null" : local.app_container_secrets
-    ulimits               = local.app_container_ulimits == "[]" ? "null" : local.app_container_ulimits
-    command               = local.app_container_command == "[]" ? "null" : local.app_container_command
-    awslogs-stream-prefix = "awslogs-${var.app_name}-pref"
-    create_datadog        = var.create_datadog
-    dd_cpu                = var.datadog_container_cpu
-    dd_memory             = var.datadog_container_memoryreservation
-    dd_container_port     = var.datadog_container_port
-    dd_name               = var.datadog_container_name
-    dd_image              = var.datadog_container_image
-    dd_api_key            = "/${data.aws_caller_identity.current.account_id}/datadog/api-key"
-    dd_environment        = local.datadog_container_environment == "[]" ? "null" : local.datadog_container_environment
-    dd_secrets            = local.datadog_container_secrets == "[]" ? "null" : local.datadog_container_secrets
-  }
-}
--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  container_definitions    = data.template_file.default-container.rendered
+  container_definitions    = local.default-container
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   cpu                      = var.task_definition_cpu
   memory                   = var.task_definition_memory
